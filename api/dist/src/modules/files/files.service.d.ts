24fd97a01219e6ddfdb33065085f0e522016dd8c
Dawson Hamera <dawsonhamera@outlook.com>
2025-11-10 19:40:39 -0800

    new changes, trying zod  typing from prisma for frontend, commiting before breaking changes

--- api/dist/src/modules/files/files.service.d.ts
+++ api/dist/src/modules/files/files.service.d.ts
@@ -2,61 +2,89 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export declare class FilesService {
     private prisma;
     constructor(prisma: PrismaService);
-    findAll(): import("@prisma/client").Prisma.PrismaPromise<{
+    findAll(): import("@prisma/client").Prisma.PrismaPromise<({
+        uploader: {
+            id: number;
+            name: string;
+            email: string;
+            password_hash: string;
+            role_id: number;
+            grade_id: number | null;
+            score: number;
+            last_active: Date | null;
+            created_at: Date | null;
+            updated_at: Date | null;
+            deleted_at: Date | null;
+        } | null;
+    } & {
+        path: string;
         id: number;
+        created_at: Date | null;
+        updated_at: Date | null;
+        deleted_at: Date | null;
+        type: import("@prisma/client").$Enums.files_type;
+        description: string | null;
         original_name: string;
         stored_name: string;
         mime_type: string | null;
         size: number;
-        path: string;
         uploaded_by: number | null;
+    })[]>;
+    findOne(id: number): import("@prisma/client").Prisma.Prisma__filesClient<({
+        uploader: {
+            id: number;
+            name: string;
+            email: string;
+            password_hash: string;
+            role_id: number;
+            grade_id: number | null;
+            score: number;
+            last_active: Date | null;
+            created_at: Date | null;
+            updated_at: Date | null;
+            deleted_at: Date | null;
+        } | null;
+    } & {
+        path: string;
+        id: number;
         created_at: Date | null;
         updated_at: Date | null;
         deleted_at: Date | null;
-        description: string | null;
         type: import("@prisma/client").$Enums.files_type;
-    }[]>;
-    findOne(id: number): import("@prisma/client").Prisma.Prisma__filesClient<{
-        id: number;
+        description: string | null;
         original_name: string;
         stored_name: string;
         mime_type: string | null;
         size: number;
-        path: string;
         uploaded_by: number | null;
-        created_at: Date | null;
-        updated_at: Date | null;
-        deleted_at: Date | null;
-        description: string | null;
-        type: import("@prisma/client").$Enums.files_type;
-    } | null, null, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
+    }) | null, null, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
     create(data: any): import("@prisma/client").Prisma.Prisma__filesClient<{
+        path: string;
         id: number;
+        created_at: Date | null;
+        updated_at: Date | null;
+        deleted_at: Date | null;
+        type: import("@prisma/client").$Enums.files_type;
+        description: string | null;
         original_name: string;
         stored_name: string;
         mime_type: string | null;
         size: number;
-        path: string;
         uploaded_by: number | null;
-        created_at: Date | null;
-        updated_at: Date | null;
-        deleted_at: Date | null;
-        description: string | null;
-        type: import("@prisma/client").$Enums.files_type;
     }, never, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
     update(id: number, data: any): Promise<{
+        path: string;
         id: number;
+        created_at: Date | null;
+        updated_at: Date | null;
+        deleted_at: Date | null;
+        type: import("@prisma/client").$Enums.files_type;
+        description: string | null;
         original_name: string;
         stored_name: string;
         mime_type: string | null;
         size: number;
-        path: string;
         uploaded_by: number | null;
-        created_at: Date | null;
-        updated_at: Date | null;
-        deleted_at: Date | null;
-        description: string | null;
-        type: import("@prisma/client").$Enums.files_type;
     }>;
     remove(id: number): Promise<void>;
 }
--- api/src/modules/users/users.service.ts
+++ api/src/modules/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
