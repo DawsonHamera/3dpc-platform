d2f1ec7ff6834011c1812160f162dad843822ad2
Dawson Hamera <dawsonhamera@outlook.com>
2025-11-18 07:27:26 -0800

    updated recyle page ui and rebuilt

--- client/dist/assets/swipe-back-legacy-DjdoBVSr.js
+++ client/dist/assets/swipe-back-legacy-DjdoBVSr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-b8BHByWO.js"],function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-DZZzcyw4.js"],function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
