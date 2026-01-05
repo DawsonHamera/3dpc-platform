b7577d2beaeb3d405a34c5e63e4eaf03954b3766
Dawson Hamera <dawsonhamera@outlook.com>
2026-01-04 21:35:01 -0800

    added back in database_url temp because of prod build error, rebuilt client

--- client/dist/assets/swipe-back-legacy-Dy9J9yJo.js
+++ client/dist/assets/swipe-back-legacy-Dy9J9yJo.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CwPGjO9A.js"],function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-DkRhj9to.js"],function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
