a50d82f658a897331211f5e2c66937a091fac794
Dawson Hamera <dawsonhamera@outlook.com>
2026-01-08 21:31:57 -0800

    Rebuilt client

--- client/dist/assets/swipe-back-legacy-S-wbEIpX.js
+++ client/dist/assets/swipe-back-legacy-S-wbEIpX.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-YbB80xcB.js"],function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
+System.register(["./index-legacy-B7_w23gx.js"],function(t,e){"use strict";var n,r,i;return{setters:[function(t){n=t.i,r=t.a,i=t.b}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
